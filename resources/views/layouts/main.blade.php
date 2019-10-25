ae579d944813e4691001a5532f52339f3fe7e286
wangyanqi <553534417@qq.com>
2019-10-25 18:11:52 +0800

    10.25

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -178,4 +178,14 @@
 </div>
 {{--<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1278136611'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s9.cnzz.com/z_stat.php%3Fid%3D1278136611%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>--}}
 </body>
+<script>
+    var _hmt = _hmt || [];
+    (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?9e4d9aa8dcb1ab93b9f6daf465ebeef5";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
+</script>
+
 </html>
