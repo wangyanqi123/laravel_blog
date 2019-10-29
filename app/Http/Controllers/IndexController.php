de63d6e086271a15b69094da41019e00a499968e
wangyanqi <553534417@qq.com>
2019-10-29 10:11:49 +0800

    baidu

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -22,33 +22,50 @@ use App\Common\Tools\MarkDowner;
 
 class IndexController extends BaseController
 {
-	//首页
-	public function index()
-	{
+    //首页
+    public function index()
+    {
         ///Redis::set('name', 'guwenjie');
         //$values = Redis::get('article_1');
         //echo $values;
+        $urls = array(
+            'http://www.wangyanqi.cc/article/11',
+            'http://www.wangyanqi.cc/article/6',
+        );
+        $api     = 'http://data.zz.baidu.com/urls?site=www.wangyanqi.cc&token=n1MZ4b6510afqDNl';
+        $ch      = curl_init();
+        $options = array(
+             CURLOPT_URL            => $api,
+             CURLOPT_POST           => true,
+             CURLOPT_RETURNTRANSFER => true,
+             CURLOPT_POSTFIELDS     => implode("\n", $urls),
+             CURLOPT_HTTPHEADER     => array('Content-Type: text/plain'),
+        );
+        curl_setopt_array($ch, $options);
+        $result = curl_exec($ch);
+        echo $result;
+
 		$list = Article::getRecent();
-		return view('index.index', ['list'=> $list]);
+		return view('index.index', ['list' => $list]);
 	}
 
-	//列表
-	public function category(Category $category, Level $level)
-	{
-		$categorys = Category::all();
-		$childs_id_arr = $level->formatChild($categorys, $category->id);
-		$list = Article::where('status', 1)->whereIn('category_id', $childs_id_arr)->orderBy('id', 'desc')->paginate(10);
+    //列表
+    public function category(Category $category, Level $level)
+    {
+        $categorys     = Category::all();
+        $childs_id_arr = $level->formatChild($categorys, $category->id);
+        $list          = Article::where('status', 1)->whereIn('category_id', $childs_id_arr)->orderBy('id', 'desc')->paginate(10);
 
-		return view('index.category', ['list'=>$list, 'category'=>$category]);
-	}
+        return view('index.category', ['list' => $list, 'category' => $category]);
+    }
 
     //详情
     public function article(Article $article)
     {
-        $markdown = new MarkDowner; //实例化
+        $markdown         = new MarkDowner; //实例化
         $article->content = $markdown->convertMarkdownToHtml($article->content); //markdown转换html
         Article::where('id', $article->id)->increment('views');
-        return view('index.article', ['post'=> $article, 'comments' => $article->comments()->with('member')->where('status', 1)->get()]);
+        return view('index.article', ['post' => $article, 'comments' => $article->comments()->with('member')->where('status', 1)->get()]);
     }
 
     /*public function article($id)
@@ -60,53 +77,53 @@ class IndexController extends BaseController
         echo "mysql结束时间<br/>";
         echo(microtime());
         exit;*/
-        /*echo "mysql开始时间<br/>";
-        echo(microtime());
-        echo "<br/>";
-        $article = Article::where('id', $id)->first();
-        echo $article->content;
-        echo "<br/>";
-        echo "mysql结束时间<br/>";
-        echo(microtime());
-        echo "redis开始时间<br/>";
-        echo(microtime());
-        echo "<br/>";
-        $redis = Redis::get('article_9');
-        print_r(json_decode($redis,true)['content']);
-        echo "<br/>";
-        echo "rerdis结束时间<br/>";
-        echo(microtime());exit;
-    }*/
+    /*echo "mysql开始时间<br/>";
+    echo(microtime());
+    echo "<br/>";
+    $article = Article::where('id', $id)->first();
+    echo $article->content;
+    echo "<br/>";
+    echo "mysql结束时间<br/>";
+    echo(microtime());
+    echo "redis开始时间<br/>";
+    echo(microtime());
+    echo "<br/>";
+    $redis = Redis::get('article_9');
+    print_r(json_decode($redis,true)['content']);
+    echo "<br/>";
+    echo "rerdis结束时间<br/>";
+    echo(microtime());exit;
+}*/
 
-	//评论
-	public function comment(Post $post, CommentRequest $request)
-	{
-		$user = Auth::user();
-		//防刷评论
-		$cache_key = $user->id;
+    //评论
+    public function comment(Post $post, CommentRequest $request)
+    {
+        $user = Auth::user();
+        //防刷评论
+        $cache_key = $user->id;
 
-		if(Cache::get($cache_key)) {
-			return redirect()->back()->with('danger', '评论的间隔时间太短');
-		}
+        if (Cache::get($cache_key)) {
+            return redirect()->back()->with('danger', '评论的间隔时间太短');
+        }
 
-		Comment::create([
-			'user_id' => $user->id,
-			'post_id' => $post->id,
-			'content' => $request->content,
-			'at_id' => 0,
-			'ip' => $request->getClientIp(),
-			'read' => 0,
-			'status' => 1
-		]);
-		Cache::put($cache_key, 'short', 1);
+        Comment::create([
+            'user_id' => $user->id,
+            'post_id' => $post->id,
+            'content' => $request->content,
+            'at_id'   => 0,
+            'ip'      => $request->getClientIp(),
+            'read'    => 0,
+            'status'  => 1
+        ]);
+        Cache::put($cache_key, 'short', 1);
 
-		return redirect()->back()->with('success', '评论成功');
-	}
+        return redirect()->back()->with('success', '评论成功');
+    }
 
-	//搜索
-	public function search(Request $request)
-	{
-		$data = Post::getSearch($request);
-		return view('index.search', $data);
-	}
+    //搜索
+    public function search(Request $request)
+    {
+        $data = Post::getSearch($request);
+        return view('index.search', $data);
+    }
 }
