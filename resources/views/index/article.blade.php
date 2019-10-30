0a2e20ae1feca99c9924cc730c1b1aa1660e3e97
wangyanqi <553534417@qq.com>
2019-10-30 15:05:48 +0800

    10.30

--- resources/views/index/article.blade.php
+++ resources/views/index/article.blade.php
@@ -10,6 +10,7 @@
 			white-space:-pre-wrap; /* Opera 4-6 */
 			white-space:-o-pre-wrap; /* Opera 7 */
 			word-wrap:break-word; /* Internet Explorer 5.5+ */
+            background: #2d2d2d;
 		}
 		 pre code {
 			display: block;
