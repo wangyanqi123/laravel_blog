41be60a6db74dc16b2ba8a8aeee3018b81b2618a
wangyanqi <553534417@qq.com>
2019-11-12 16:52:58 +0800

    11.12

--- resources/views/index/search.blade.php
+++ resources/views/index/search.blade.php
@@ -1,6 +1,6 @@
 
 @extends('layouts.main')
-@section('title', '关键词:'.$search.'的结果')
+{{--@section('title', '关键词:'.$search.'的结果')--}}
 
 @section('content')
 	@include('index._article', ['list'=> $list])
