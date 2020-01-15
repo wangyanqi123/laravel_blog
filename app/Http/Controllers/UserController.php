1105bd8b9ae4e71272e268bd3c6e8bd66dc02380
wangyanqi <553534417@qq.com>
2020-01-15 10:47:46 +0800

    1.15

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,7 +22,6 @@ class UserController extends BaseController
 
 	public function store(UserRequest $request)
 	{
-	    echo 123;exit;
 		$user = User::add([
 			'username' => $request->username,
 			'email' => $request->email,
